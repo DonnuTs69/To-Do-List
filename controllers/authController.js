a275b903e03933066a078f6663cf560f620ac8b4
DonnuTs69 <febrykurniawanhakim@gmail.com>
2023-04-06 20:08:45 +0700

    login and refresh token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const db = require("../models")
 const bcrypt = require("bcrypt")
 const { Op } = require("sequelize")
+const { signToken } = require("../lib/jwt")
 
 const authController = {
   registerUser: async (req, res) => {
@@ -31,6 +32,72 @@ const authController = {
       return res.status(201).json({
         message: "User registered",
         data: newUser,
+      })
+    } catch (err) {
+      console.log(err)
+      return res.status(500).json({
+        message: err.message,
+      })
+    }
+  },
+  loginUser: async (req, res) => {
+    try {
+      const findUserByUsernameOrEmail = await db.User.findOne({
+        where: {
+          [Op.or]: {
+            username: req.body.usernameOrEmail,
+            email: req.body.usernameOrEmail,
+          },
+        },
+      })
+
+      if (!findUserByUsernameOrEmail) {
+        return res.status(400).json({
+          message: "User not found",
+        })
+      }
+
+      const passwordIsValid = bcrypt.compareSync(
+        req.body.password,
+        findUserByUsernameOrEmail.password
+      )
+
+      if (!passwordIsValid) {
+        return res.status(400).json({
+          message: "Password invalid",
+        })
+      }
+
+      delete findUserByUsernameOrEmail.dataValues.password
+
+      const token = signToken({
+        id: findUserByUsernameOrEmail.id,
+      })
+
+      return res.status(201).json({
+        message: "User Login",
+        data: findUserByUsernameOrEmail,
+        token,
+      })
+    } catch (err) {
+      console.log(err)
+      return res.status(500).json({
+        message: err.message,
+      })
+    }
+  },
+  refreshToken: async (req, res) => {
+    try {
+      const findUserById = await db.User.findByPk(req.user.id)
+
+      const renewedToken = signToken({
+        id: req.user.id,
+      })
+
+      return res.status(200).json({
+        message: "Renewed user token",
+        data: findUserById,
+        token: renewedToken,
       })
     } catch (err) {
       console.log(err)
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const authController = require("../controllers/authController")
 const router = express.Router()
+const { verifyToken } = require("../middlewares/authMiddleware")
 
 router.post("/register", authController.registerUser)
+router.post("/login", authController.loginUser)
+router.get("/refresh-token", verifyToken, authController.refreshToken)
 
 module.exports = router
